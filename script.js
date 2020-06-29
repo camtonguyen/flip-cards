651e7a22efa77bde166b5c643c6677e26fb360c8
Nguyen Thi Cam To <camto2831997@gmail.com>
2020-06-30 03:15:36 +0700

    update service worker

--- script.js
+++ script.js
@@ -141,11 +141,9 @@ clearBtn.addEventListener('click', () => {
   window.location.reload();
 });
 
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function() {
-    navigator.serviceWorker
-      .register("/serviceWorker.js")
-      .then(res => console.log("service worker registered"))
-      .catch(err => console.log("service worker not registered", err))
-  })
-}
+window.addEventListener("load", function() {
+  navigator.serviceWorkerContainer.register
+    .register("/serviceWorker.js")
+    .then(res => console.log("service worker registered"))
+    .catch(err => console.log("service worker not registered", err))
+});
